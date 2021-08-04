ae0db6f1ab8c0223b2e3b0de373d7bfa4790861a
Mario Conti <marioConti91@gmail.com>
2021-08-04 11:56:26 -0300

    se crea el repositorio. Se suben archivos y dependencias de React.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import HotelInfo from "./recursos/HotelInfo";
+
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <HotelInfo />
+      </>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/recursos/HotelInfo/HotelInfo.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+// import { hotelsData } from "../arrayhoteles";
+
+
+// Esta función es la que hara la instanciación dinámica de las card con HOTEL INFO
+// Componente
+export const ListadoHoteles = () => {
+    return <> 
+    </>
+}
+
+// Esta función será la card genérica que tendrá el html y css y recibirá por props la info de los hoteles
+export const HotelInfo = () => {
+  return <>
+  </>;
+};
+
