07913131ae9d3e6e361a89e383722260336f12ea
Mario Conti <marioConti91@gmail.com>
2021-08-09 11:58:59 -0300

    se agrega signos pesos y modificaciones en el css

--- src/recursos/HotelInfo/HotelInfo.jsx
+++ src/recursos/HotelInfo/HotelInfo.jsx
@@ -22,7 +22,7 @@ export const ListadoHoteles = () => {
       </>
     );
   });
-  return <div className="contenedor-section-hotel">{lista};</div>;
+  return <div className="contenedor-section-hotel">{lista}</div>;
 };
 
 // Esta función será la card genérica que tendrá el html y css y recibirá por props la info de los hoteles
@@ -36,12 +36,20 @@ export const HotelInfo = (props) => {
   // Desde
   // Hasta
   let fechaHastaUnix = props.hasta;
-
   let diaHasta = new Date(fechaHastaUnix).getDate();
   let nombreDiaHasta = new Date(fechaHastaUnix).getDay();
   let nombreMesHasta = new Date(fechaHastaUnix).getMonth();
   let nombreAñoHasta = new Date(fechaHastaUnix).getFullYear();
   // Hasta
+
+  function mostrarPrice() {
+    let signoPesos = <i class="fas fa-dollar-sign"></i>;
+    let arrayPesos = [];
+    for (let i = 0; i < props.price; i++) {
+      arrayPesos.push(signoPesos);
+    }
+    return arrayPesos;
+  }
   // esta función genérica recupera el valor del numero del día y devuelve el nombre
   const nombreDia = (e) => {
     if (e === 0) {
@@ -107,21 +115,7 @@ export const HotelInfo = (props) => {
       <div className="rectangulos">
         <div className="country">
           <div className="svg">
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fas"
-              data-icon="map-marker"
-              class="svg-inline--fa fa-map-marker fa-w-12"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 384 512"
-            >
-              <path
-                fill="currentColor"
-                d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z"
-              ></path>
-            </svg>
+            <i class="fas fa-map-marker"></i>
           </div>
           <p className="texto-rectangulo">
             {props.country}, {props.city}
@@ -130,44 +124,12 @@ export const HotelInfo = (props) => {
 
         <div className="habitaciones">
           <div className="svg">
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fas"
-              data-icon="bed"
-              class="svg-inline--fa fa-bed fa-w-20"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 640 512"
-            >
-              <path
-                fill="currentColor"
-                d="M176 256c44.11 0 80-35.89 80-80s-35.89-80-80-80-80 35.89-80 80 35.89 80 80 80zm352-128H304c-8.84 0-16 7.16-16 16v144H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v352c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16v-48h512v48c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V240c0-61.86-50.14-112-112-112z"
-              ></path>
-            </svg>
-            {/* Como importar este svg desde otro archivo externo para que no se arme esto por cada svg */}
+            <i class="fas fa-bed"></i>
           </div>
           <p className="texto-rectangulo">{props.rooms} habitaciones</p>
         </div>
         <div className="precio">
-          <div className="svg">
-            <mostrarPrecios />
-            {/* <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fas"
-              data-icon="dollar-sign"
-              class="svg-inline--fa fa-dollar-sign fa-w-9"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 288 512"
-            >
-              <path
-                fill="currentColor"
-                d="M209.2 233.4l-108-31.6C88.7 198.2 80 186.5 80 173.5c0-16.3 13.2-29.5 29.5-29.5h66.3c12.2 0 24.2 3.7 34.2 10.5 6.1 4.1 14.3 3.1 19.5-2l34.8-34c7.1-6.9 6.1-18.4-1.8-24.5C238 74.8 207.4 64.1 176 64V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48h-2.5C45.8 64-5.4 118.7.5 183.6c4.2 46.1 39.4 83.6 83.8 96.6l102.5 30c12.5 3.7 21.2 15.3 21.2 28.3 0 16.3-13.2 29.5-29.5 29.5h-66.3C100 368 88 364.3 78 357.5c-6.1-4.1-14.3-3.1-19.5 2l-34.8 34c-7.1 6.9-6.1 18.4 1.8 24.5 24.5 19.2 55.1 29.9 86.5 30v48c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-48.2c46.6-.9 90.3-28.6 105.7-72.7 21.5-61.6-14.6-124.8-72.5-141.7z"
-              ></path>
-            </svg> */}
-          </div>
+          <div className="svg">{mostrarPrice()}</div>
         </div>
       </div>
 
