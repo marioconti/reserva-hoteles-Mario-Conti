8bb67c18cded00a55e2984fcda928a17c58a5a1b
Mario Conti <marioConti91@gmail.com>
2021-08-10 12:25:33 -0300

    se agrega select pais

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import { React } from "react";
 import "./App.css";
 import { ListadoHoteles } from "./recursos/HotelInfo/HotelInfo";
 import { Header } from "./recursos/Header/Header";
 import { Filtros } from "./recursos/Filtros/Filtros";
 
 function App() {
+  // const [country, setCountry] = useState("");
+
   return (
     <div className="App">
       <Header />
--- src/recursos/Filtros/Filtros.jsx
+++ src/recursos/Filtros/Filtros.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 // import { hotelsData } from "../arrayhoteles";
 import "./Filtros.css";
+import App from "../../App";
 
 // Componente de los Filtros
 export const Filtros = () => {
   return (
     <div className="contenedor-filtros">
-      <input className="filtro" type="text" name="" id="" />
-      <input className="filtro" type="text" name="" id="" />
-      <input className="filtro" type="text" name="" id="" />
-      <input className="filtro" type="text" name="" id="" />
-      <input className="filtro" type="text" name="" id="" />
+      <FiltroPais />
+    </div>
+  );
+};
+/* =========================================================== */
+
+// Filtro por Pais
+export const FiltroPais = () => {
+  return (
+    <div className="filtros">
+      <select className="filtrosPorPais" name="Todos" id="">
+        <option value="Todos">Todos los hoteles</option>
+        <option value="Argentina">Argentina</option>
+        <option value="Brasil">Brasil</option>
+        <option value="Chile">Chile</option>
+        <option value="Uruguay">Uruguay</option>
+      </select>
     </div>
   );
 };
